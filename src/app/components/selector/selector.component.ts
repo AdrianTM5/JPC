1172f31141d2fb3e2b75274b9fb95f7d6c223e56
angel adrian monsivais lopez <angel220803@hotmail.com>
2023-11-01 17:44:04 -0600

    Primer commit con diseño principal pero sin funcionalidad

---
+++ src/app/components/selector/selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.css']
+})
+export class SelectorComponent {
+
+}
